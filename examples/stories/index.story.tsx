d3363c5283b226a71249a29053d3ca6ba6456c88
foryuki <foryuki@outlook.com>
2018-11-03 12:52:01 +0800

    build: update scaffold to or-template@v2.4.1

--- examples/stories/index.story.tsx
+++ examples/stories/index.story.tsx
@@ -1,15 +1,33 @@
+import { action } from '@storybook/addon-actions'
 import { withInfo } from '@storybook/addon-info'
 import { storiesOf } from '@storybook/react'
 import React from 'react'
 
-import Example from './example.story'
+import Switch from 'or-switch'
+import { previewCode } from './util'
+
+import Example from './example'
+
 import './styles.scss'
 
-storiesOf('or-switch', module).add(
-  'switch',
-  withInfo({ inline: true })(() => (
-    <div>
-      <Example />
-    </div>
-  ))
-)
+const handleClick = action('basic-click')
+storiesOf('or-switch', module)
+  .addDecorator(
+    withInfo({
+      inline: true,
+      propTables: [Switch],
+      propTablesExclude: [Example],
+      styles: {
+        jsxInfoContent: {
+          borderTop: 'none',
+          margin: 0
+        }
+      }
+    })
+  )
+  .add('sample', () => <Example />, {
+    info: {
+      source: false,
+      text: previewCode(require('!!raw-loader!./example.tsx'))
+    }
+  })
